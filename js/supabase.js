5600d87783e73c3b2b5e3bd18b87a9b45d40d651
Mayank-14uttam <mayankuttam733@gmail.com>
2026-07-22 01:25:17 +0530

    Enhance Supabase client initialization
    Added configuration options for session persistence and token management.

--- js/supabase.js
+++ js/supabase.js
@@ -7,11 +7,16 @@ const SUPABASE_URL = "https://hdxbdrndajvnsrrsfkiq.supabase.co";
 
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhkeGJkcm5kYWp2bnNycnNma2lxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQzNjk0NjYsImV4cCI6MjA5OTk0NTQ2Nn0.Kcp_nIHfVF4kQendMsxns_OeaCPO_CV-0-64UDiwD6U";
 
-
 const supabaseClient = supabase.createClient(
     SUPABASE_URL,
-    SUPABASE_ANON_KEY
+    SUPABASE_ANON_KEY,
+    {
+        auth:{
+            persistSession:true,
+            autoRefreshToken:true,
+            detectSessionInUrl:true
+        }
+    }
 );
-
 
 console.log("✅ Supabase Connected");
